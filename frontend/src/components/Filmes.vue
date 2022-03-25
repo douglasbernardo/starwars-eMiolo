e31748d6bf6b095da3df0d368b9ce8d8830244a8
Douglas Bernardo de Melo <douglasbernardomelo@gmail.com>
2022-03-25 19:57:40 -0300

    mostrando os filmes do starwars

--- frontend/src/components/Filmes.vue
+++ frontend/src/components/Filmes.vue
@@ -1,6 +1,18 @@
 <template>
     <div>
-        <p>{{filmes}}</p>
+        <div class="container">
+            <div class="box" v-for="filme in filmes" :key="filme.id">
+                <p> Título:{{filme.title}}</p>
+                <p> Descrição:{{filme.opening_crawl}}</p> 
+                <p> Data:{{filme.release_date}}</p>
+            </div>
+        </div>
+        <!-- <p v-for="filme in filmes" :key="filme.id">
+            {{filme.title}} - {{filme.opening_crawl}} - {{filme.release_date}}
+            title,
+            opening_crawl,
+            release_data, 
+        </p> -->
     </div>
 </template>
 
@@ -29,4 +41,28 @@ import axios from "axios"
 
 <style scoped>
 
+
+    .container{
+        display: flex;
+        width: 100%;
+        flex-wrap: wrap;
+        flex-direction: row;
+    }
+    .box{
+        display: flex;
+        height: 500px;
+        flex-wrap: wrap;
+        width: 30%;
+        margin: 15px;
+        padding:10px;
+        justify-items: center;
+        justify-content: center;
+        background: black;
+    }
+
+    .box p{
+        margin:10px;
+        color: antiquewhite;
+        font-size: 20px;
+    }
 </style>
